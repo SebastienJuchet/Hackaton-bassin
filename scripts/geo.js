c343c3a37b5c70c4165f73e7bd0cf48629f706b3
Sebastien Juchet <sebastien.juchet@gmail.com>
2021-11-26 11:31:40 +0100

    Fix error

--- scripts/geo.js
+++ scripts/geo.js
@@ -1,13 +1,16 @@
-let coord = []
+let coord = [];
+let currentSpeed;
+let seconde = 0;
+let divSpeed = document.getElementById('speed');
 let successHandler = function(position) {
     coord['lat'] = position.coords.latitude;
     coord['long'] = position.coords.longitude;
 };
 
 let errorHandler = function (errorObj) {
-    alert(errorObj.code + ": " + errorObj.message);
-    alert("something wrong take this lat " + "26.0546106" );
-    alert("something wrong take this lng " +-98.3939791);
+    console.log(errorObj.code + ": " + errorObj.message);
+    console.log("something wrong take this lat " + "26.0546106" );
+    console.log("something wrong take this lng " +-98.3939791);
 };
 
 navigator.geolocation.getCurrentPosition(
@@ -15,4 +18,49 @@ navigator.geolocation.getCurrentPosition(
     {enableHighAccuracy: true, maximumAge: 10000}
 );
 
-console.log(coord)
+let prevLat = 44.84;
+let prevLong = -0.56;
+let successHandlerTime = function(position) {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    console.log(long, lat)
+    let diff = getDistance(
+        {lat: prevLat, lon: prevLong},
+        {lat: lat, lon: long},
+        {unit: 'meters'}
+    );
+    currentSpeed = diff / 1000 * 3600;
+    prevLat = lat;
+    prevLong = long;
+};
+setInterval(() => {
+    seconde++
+    navigator.geolocation.getCurrentPosition(
+        successHandlerTime, errorHandler,
+        {enableHighAccuracy: true, maximumAge: 10000}
+    );
+    divSpeed.innerHTML = `Votre vitesse est de ${currentSpeed ?? 0} km/h`
+    let averageSpeed = 0;
+    if (currentSpeed !== 0 ) {
+        averageSpeed += currentSpeed
+    }
+    if (seconde === 10) {
+        averageSpeed /= 10
+        seconde = 0
+        //console.log(isNaN(averageSpeed))
+        if (averageSpeed < speeds[level.value][0]) {
+            while (averageSpeed < speeds[level.value][0]) {
+                level.value --;
+                valueLevel.innerHTML = vitesse[level.value];
+                //console.log(musics[level.value])
+            }
+        }
+        if (averageSpeed > speeds[level.value][1]) {
+            while (averageSpeed > speeds[level.value][1]) {
+                level.value ++;
+                valueLevel.innerHTML = vitesse[level.value];
+                //console.log(musics[level.value])
+            }
+        }
+    }
+}, 1000)
